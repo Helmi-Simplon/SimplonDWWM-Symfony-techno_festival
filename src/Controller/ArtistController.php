32fa3c155b6952ffd661bacc30d685849fe7db0a
Helmi-Simplon <shan.tunisois@gmail.com>
2021-06-21 19:01:06 +0200

    afficher les artistes dans la page artiste + afficher la page détail artiste

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -11,10 +11,22 @@ class ArtistController extends AbstractController
     /**
      * @Route("/artist", name="artist")
      */
-    public function index(): Response
+    public function artist(): Response
     {
+        $color = ["secondary", "danger", "info", "warning", "light", "success"];
         return $this->render('artist/artist.html.twig', [
             'controller_name' => 'ArtistController',
+            'colors' => $color,
+            ]);
+    }
+
+    /**
+     * @Route("/artist/{id}", name="artist_id", methods={"GET"}, requirements={"id"="\d+"})
+     */
+    public function artist_id($id): Response
+    {
+        return $this->render('artist/detailartist.html.twig', [
+            'id' => $id,
         ]);
     }
 }
